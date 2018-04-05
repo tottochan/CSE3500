be8424c2a0aec82bb30bf4c44c7dd302ac4532d1
tottochan <vannguyenthanh@gmail.com>
2018-04-05 16:32:32 -0400

    min_coin

---
+++ dynamic-programming/min_coin.py
@@ -0,0 +1,31 @@
+def min_coin(D, n):
+    
+    C = [ 0 for i in range(n+1)]
+
+    C[0] = 0
+
+    for i in range(1, n+1):
+        
+        C[i] = -1
+
+        for j in range(len(D)):
+            
+            if i >= D[j] and C[i - D[j]] >= 0:
+
+                if C[i] == -1:
+                    
+                    C[i] = C[i - D[j]] + 1
+                    
+                elif C[i] > C[i - D[j]] + 1:
+                    
+                    C[i] = C[i - D[j]] + 1
+                    
+    return C[n]
+
+D = [1, 2, 5, 7]
+
+n = 100
+
+m = min_coin(D,n)
+
+print(m)
